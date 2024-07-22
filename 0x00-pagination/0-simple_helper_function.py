748683c65612358ecca26f255f011c1a027495fe
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-22 23:11:06 +0000

    werid

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,7 +6,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ containing a start index and an end index corresponding to the
-         range of indexes to return in a list for 
+         range of indexes to return in a list for
         those particular pagination parameters """
     start_index = (page - 1) * page_size
     end_index = page * page_size
