18cc2d28c82feee44917fe94baca4582ccd48365
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-22 23:14:00 +0000

    documentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -23,6 +23,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Returns a  particular page using start and end"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         rows = []
