ece946c19bc297c1f4cbc5ed3c21015d3d782c12
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-23 01:35:08 +0000

    trailing whitespace

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -41,7 +41,7 @@ class Server:
         """ Returns a dictinary containing the ff:
             page_size: the length of the returned dataset page
             page: the current page number
-            data: the dataset page 
+            data: the dataset page
             next_page: number of the next page, None if no next page
             prev_page: number of the previous page, None if no previous page
             total_pages: the total number of pages in the dataset as an integer
