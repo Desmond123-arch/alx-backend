ddf858591a57f3799aec9f5d3e514ac3c206eaf0
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-30 23:59:07 +0000

    basic template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """ index page """
+    return render_template('0-index.html')
