f4eb0a698012e6adb2fa7d8e95abc78d1edff264
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-31 00:29:08 +0000

    added babel constants

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,6 +7,9 @@ from flask_babel import Babel
 class Config:
     """ Config class for app"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 
 app = Flask(__name__)
