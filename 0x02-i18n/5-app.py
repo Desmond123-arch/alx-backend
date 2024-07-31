5ee7dcb0b4f77b9e06a748d6f006e0f7335f88c5
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-31 01:28:22 +0000

    task-5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ Config class for app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get langauge code of user"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user() -> Union[dict, None]:
+    '''returns a user dictionary or None'''
+    if request.args.get('login_as'):
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
+    else:
+        return None
+
+
+@app.before_request
+def before_request():
+    '''to find a user if any'''
+    g.user = get_user()
+
+
+@app.route('/')
+def index_page() -> str:
+    """ index page """
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
