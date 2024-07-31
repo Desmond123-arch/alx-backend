28fa26b46bb90cc6b64422a4e6c9b901212ceb7e
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-31 00:07:36 +0000

    weird

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,11 +2,16 @@
 """ Basic flask app """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
 
 @app.route("/")
 def index():
     """ index page """
     return render_template('0-index.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
