aeb72dcbdd30d24dbe6bf450f34a26bb64ace583
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-31 00:57:11 +0000

    missed something?

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
-""" Basic flask app """
+""" basic flask project"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """ Config class for app"""
+    """ Config class for app """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.config.from_object(Config)
-babel = Babel(app, default_locale="en", default_timezone="UTC")
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """Get langauge code of user"""
+def get_locale() -> str:
+    """ Get locale from request """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/')
-def index_page() -> str:
-    """ index page """
+@app.route("/")
+def welcome() -> str:
+    """ / page """
     return render_template('3-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
