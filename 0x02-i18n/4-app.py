7d9588c6af12605c89f6d7a288ab9e4fdd0e4278
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-31 01:10:22 +0000

    fixed locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -14,14 +14,14 @@ class Config:
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
-babel = Babel(app, default_locale="en", default_timezone="UTC")
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """Get langauge code of user"""
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
@@ -29,8 +29,7 @@ def get_locale():
 @app.route('/')
 def index_page() -> str:
     """ index page """
-
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
